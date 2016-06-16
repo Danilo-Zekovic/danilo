03e4f0a9a40bdb8264cce5afa2a4d5876d07b2e3
Danilo Zekovic <danilozeka93@gmail.com>
2016-06-16 18:51:08 -0400

    created navbar

--- js/modules/App.js
+++ js/modules/App.js
@@ -5,12 +5,33 @@ export default React.createClass({
   render() {
     return (
       <div>
-        <h1>React Router Tutorial</h1>
-        <ul role="nav">
-          <li><NavLink to="/" onlyActiveOnIndex>Home</NavLink></li>
-          <li><NavLink to="/about">About</NavLink></li>
-          <li><NavLink to="/repos">Repos</NavLink></li>
-        </ul>
+        <nav className="navbar navbar-custom navbar-fixed-top" role="navigation">
+          <div className="container">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                <i className="fa fa-bars"></i>
+              </button>
+              <a className="navbar-brand page-scroll" href="#page-top">
+                <i className="fa fa-play-circle"></i>  <span className="light">Start</span> Bootstrap
+              </a>
+            </div>
+
+
+            <div className="collapse navbar-collapse navbar-right navbar-main-collapse">
+              <ul className="nav navbar-nav">
+
+                <li className="hidden">
+                  <a href="#page-top"></a>
+                </li>
+                <li><NavLink to="/" onlyActiveOnIndex>Home</NavLink></li>
+                <li><NavLink to="/about">About</NavLink></li>
+                <li><NavLink to="/repos">Repos</NavLink></li>
+              </ul>
+            </div>
+
+          </div>
+
+        </nav>
         {this.props.children}
       </div>
     )
