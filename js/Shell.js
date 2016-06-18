b30d142e6986cf370f11eb9c55dacf3c4cf04f6f
Danilo Zekovic <danilozeka93@gmail.com>
2016-06-18 01:00:29 -0400

    adjusted so it works

--- js/Shell.js
+++ js/Shell.js
@@ -6,7 +6,7 @@
   */
 
 import React from 'react'
-import { render } from 'react-dom'
+import ReactDOM from 'react-dom'
 import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import App from './modules/App'
 import About from './modules/About'
@@ -16,7 +16,7 @@ import Home from './modules/Home'
 
 import Header from './modules/Header'
 
-render((
+ReactDOM.render((
   <Router history={browserHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={Home}/>
@@ -24,7 +24,7 @@ render((
         <Route path="/repos/:userName/:repoName" component={Repo}/>
       </Route>
       <Route path="/about" component={About}/>
-    <Route path="/h" component={Header}/>
+    <Route path="/h" component={About}/>
     </Route>
   </Router>
 ), document.getElementById('app'))
