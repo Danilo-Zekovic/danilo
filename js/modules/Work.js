d630c115cab1ff8e8cd1e07795075cf81a294904
Danilo Zekovic <danilozeka93@gmail.com>
2016-06-20 16:58:07 -0400

    place to have the experience; just example for now

---
+++ js/modules/Work.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export default React.createClass({
+  render() {
+    return (
+      <div>
+        <div className="user-intro foo2 container-fluid text-center">
+          <img className="img-circle img-responsive text-center github-logo" src='http://2.bp.blogspot.com/-GIS3_Ms5uHg/T8pLfIYZGNI/AAAAAAAAT48/FCFXTOdBsJs/s1600/young-justice-nightwing-robin-wallpaper.jpg'/>
+          <h1>Work</h1>
+        </div>
+
+        <div className="container-fluid">
+          <div className="container text-center">
+            <h2 className="subtitle">Nightwing Report</h2>
+            <p>Although I aspire to one day change the world with my code, one
+              of the biggest passions in my life is learning. I want to start my
+              career by tackling problems for my clients and growing in my
+              experience with each line of code. I enjoy expressing creativity,
+              which I hope to use in creating software that impacts people's
+              lives.
+            </p>
+          </div>
+        </div>
+
+      </div>
+    )
+  }
+})
