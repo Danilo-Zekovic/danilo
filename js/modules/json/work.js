07c65753e0621c196162ad1890a891322dbb4a92
Danilo Zekovic <danilozeka93@gmail.com>
2016-07-19 16:43:56 -0400

    json with experience

---
+++ js/modules/json/work.js
@@ -0,0 +1,41 @@
+const work = [{
+	background: '#2980B9',
+	imgSrc: 'https://s3.amazonaws.com/uifaces/faces/twitter/rem/128.jpg',
+	imgBorderColor: '#015389',
+
+	date: 'May 17, 2016',
+	title: 'O’Reilly OSCON',
+	jobTitle: 'Tutorial',
+	position: 'Co-Presenter',
+	location: 'Austin, TX',
+	technologies: 'JavaScript, React, Node.js, Express, Babel, Webpack, GrpahQL',
+	project: 'foo bar'
+},
+{
+	background: 'blue',
+	imgSrc: 'https://s3.amazonaws.com/uifaces/faces/twitter/rem/128.jpg',
+	imgBorderColor: '#015389',
+
+	date: 'May-June 2015',
+	title: 'HintTech',
+	jobTitle: 'Internship',
+	position: 'Intern',
+	location: 'Novi Sad, Serbia',
+	technologies: 'C#, Visual Studio, JavaScript',
+	project: 'I am focused on keeping up with new technologies. The notion of a	“web page” is undergoing revolutionary change, lines are blurring	between client and server, and the importance of JavaScript	continues to grow. I cannot predict how these things will come together, but I know this is the path that I want to be on.'
+},
+{
+	background: 'red',
+	imgSrc: 'https://s3.amazonaws.com/uifaces/faces/twitter/rem/128.jpg',
+	imgBorderColor: '#015389',
+
+	date: 'June 2014',
+	title: 'DMS Schneider Electic',
+	jobTitle: 'Internship',
+	position: 'Intern',
+	location: 'Novi Sad, Serbia',
+	technologies: 'Java, Eclipse, XML',
+	project: 'Desktop app that has as input and output a XML file. It	translates words in the XML file from/to English or Spanish. I was provided with CSV file that contained English words and its	translations to Spanish. Input files contained at least 20k or 30k words that needed to be translated.'
+}];
+
+export default work;
