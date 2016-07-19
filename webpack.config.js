f9f00e8b6513a57561c2298504a345917389420d
Danilo Zekovic <danilozeka93@gmail.com>
2016-07-19 16:45:07 -0400

    made it read jsx

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   module: {
     loaders: [
       {
-        test: /\.js$/,
+        test: /\.jsx?|\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader?presets[]=es2015&presets[]=react'
       },
